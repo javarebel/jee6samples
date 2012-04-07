34e1a3158c6bb9148f87f1ec9c8cf1303c1e5ad0
Naveen <naveensisupalan@yahoo.com>
2012-04-07 00:01:59 -0600

    Initial Version

---
+++ sample-util/src/main/java/org/naveen/jee6/samples/TestUtil.java
@@ -0,0 +1,8 @@
+package org.naveen.jee6.samples;
+
+public class TestUtil {
+
+	public static String format(String message) {
+		return String.format("This is a formatted message. %s", message);
+	}
+}
